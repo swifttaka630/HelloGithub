d1a6763d09bb0a1472f2653f7f51d8c16b623338
Takato <takatakato807@gmail.com>
2019-11-11 16:31:13 +0900

    Initial Commit

---
+++ HelloGithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloGithub
+//
+//  Created by 板垣誉士 on 2019/11/11.
+//  Copyright © 2019 板垣誉士. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
